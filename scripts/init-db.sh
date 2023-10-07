f5bf94fd2e1363bb7d89cc64775101393037fee8
marcosrosse <marcosrossem@gmail.com>
2023-10-07 19:49:58 +0100

    first database and table creation

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -3,7 +3,9 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE proddatabase;
+EOSQL
 
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "proddatabase" <<-EOSQL
 	CREATE TABLE IF NOT EXISTS avatars (
 	id SERIAL PRIMARY KEY,
 	path VARCHAR
